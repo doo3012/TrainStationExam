412547bddde84a4e813d6f86a39c7dcc8c637e59
train heartnet <naratorndoo@gmail.com>
2026-05-25 19:11:47 +0700

    feat: initialize axios instance with base URL and HTTP method constants

--- api/TrainStationApi/Extensions/ServiceCollectionExtensions.cs
+++ api/TrainStationApi/Extensions/ServiceCollectionExtensions.cs
@@ -29,7 +29,12 @@ public static class ServiceCollectionExtensions
             options.AddPolicy("Frontend", policy =>
             {
                 policy
-                    .WithOrigins("http://localhost:3000")
+                    .WithOrigins(
+                        "http://localhost:3000",
+                        "http://localhost:3001",
+                        "http://127.0.0.1:3000",
+                        "http://127.0.0.1:3001"
+                    )
                     .AllowAnyHeader()
                     .AllowAnyMethod();
             });
